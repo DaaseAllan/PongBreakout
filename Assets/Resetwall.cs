1d531e28d1be9b314baba1c2c828ead1a8075a18
Laurits Lauritsen <laurits.mumberg@gmail.com>
2016-03-16 19:15:03 +0100

    resetwalls tak
    tak ja tak

--- Assets/Resetwall.cs
+++ Assets/Resetwall.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SideWalls : MonoBehaviour {
+public class Resetwall : MonoBehaviour {
 
 
 	void OnTriggerEnter2D (Collider2D hitInfo) {
 		if (hitInfo.name == "Ball") {
-			string wallName = transform.name;
 			hitInfo.gameObject.SendMessage ("resetBall", 0.05, SendMessageOptions.RequireReceiver);
 			Debug.Log("RESEEEEEEEET!!!!");
 		}
