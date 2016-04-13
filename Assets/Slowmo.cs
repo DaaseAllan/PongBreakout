8560e958769736e07b98a393827caa69705a9dc4
DaaseAllan <laurits.mumberg@gmail.com>
2016-04-13 17:36:40 +0200

    Exploder + Flere blocks
    Tak for et dejligt asset jeg ikke har retten til :^)

--- Assets/Slowmo.cs
+++ Assets/Slowmo.cs
@@ -11,7 +11,7 @@ public class Slowmo : MonoBehaviour {
 		while (true)
 		{
 			GetComponent<Camera> ().backgroundColor = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
-			yield return WaitForSeconds (0.1f);
+			yield return new WaitForSeconds (0.1f);
 
 		}
 	
